a5569b5c7c71fa5e393fc170ed15518d0cd5d176
shaz.ahmed <steve.josh@kanrad.com>
2024-07-30 16:25:44 +0530

    shadcn button added

--- microsoft-loop-clone/app/page.js
+++ microsoft-loop-clone/app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>My Projects</h2>
+      <Button variant="outline" >New Button</Button>
     </div>
   );
 }
