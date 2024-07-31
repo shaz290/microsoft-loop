7a9a71cc6674f0121ea543786b25e86cd6a4fce5
shaz.ahmed <steve.josh@kanrad.com>
2024-07-31 13:48:05 +0530

    orgnation deletion

--- microsoft-loop-clone/app/(routes)/_components/Header.jsx
+++ microsoft-loop-clone/app/(routes)/_components/Header.jsx
@@ -1,14 +1,20 @@
+"use client"
 import { Logo } from '@/app/_components/Logo'
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
+import { OrganizationSwitcher, useAuth, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Header = () => {
+  const { orgId } = useAuth();
+  console.log(orgId);
+
   return (
     <div className='flex justify-between items-center p-3
     shadow-sm '>
-      <Logo/>
-      <OrganizationSwitcher/>
-      <UserButton/>
+      <Logo />
+      <OrganizationSwitcher
+        afterLeaveOrganizationUrl={'/dashboard'}
+        afterCreateOrganizationUrl={'/dashboard'} />
+      <UserButton />
     </div>
   )
 }
