9aef1e0f36145e8adaa92510bf0a0b38b12391cb
shaz.ahmed <steve.josh@kanrad.com>
2024-07-31 14:39:59 +0530

    workspace page

---
+++ microsoft-loop-clone/app/(routes)/_components/WorkSpaceList.jsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { Button } from '@/components/ui/button';
+import { useUser } from '@clerk/nextjs'
+import { AlignLeft, LayoutGrid } from 'lucide-react';
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+const WorkSpaceList = () => {
+
+    const { user } = useUser();
+
+    const [workspaceList, setWorkspaceList] = useState([]);
+
+    return (
+        <div className='my-10 p-10 md:px-24 lg:px-36 xl:px-52:'
+
+        >
+            <div className='flex justify-between'>
+                <h2
+                    className='font-bold text-2xl'
+                    style={{
+                        color: 'skyblue',
+                        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)',
+                        fontFamily: "'Brush Script MT', cursive",
+                        letterSpacing: '1px',
+                        lineHeight: '1.5'
+                    }}
+                >
+                    Hello, {user?.fullName}
+                </h2>
+                <Button>+</Button>
+            </div>
+            <div className='mt-10 flex justify-between'>
+                <div >
+                    <h2 className='font-medium text-primary'>
+                        Workspaces
+                    </h2>
+                </div>
+                <div className='flex gap-2'>
+                    <LayoutGrid />
+                    <AlignLeft />
+                </div>
+            </div>
+
+            {workspaceList?.length == 0 ?
+                <div className='flex flex-col justify-center items-center my-10'>
+                    <Image src={'/createfile.png'}
+                        width={200} height={200}
+                        alt='workspace' />
+                    <h2>
+                        Create a new workspace
+                    </h2>
+                    <Button variant="outline"
+                        className='my-3'
+                    >
+                        + New Workspace
+                    </Button>
+                </div>
+                :
+                <div>
+                    WorkSpaceList
+                </div>
+            }
+        </div>
+    )
+}
+
+export default WorkSpaceList
--- microsoft-loop-clone/app/(routes)/dashboard/page.js
+++ microsoft-loop-clone/app/(routes)/dashboard/page.js
@@ -1,11 +1,13 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import Header from '../_components/Header'
+import WorkSpaceList from '../_components/WorkSpaceList'
 
 const Dashboard = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <WorkSpaceList />
     </div>
   )
 }
