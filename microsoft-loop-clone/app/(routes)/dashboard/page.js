ee15287802ba20ca68960d7df916dda0b9ef9dad
shaz.ahmed <steve.josh@kanrad.com>
2024-07-31 12:46:23 +0530

    added logo in the header

--- microsoft-loop-clone/app/(routes)/dashboard/page.js
+++ microsoft-loop-clone/app/(routes)/dashboard/page.js
@@ -1,10 +1,11 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import Header from '../_components/Header'
 
 const Dashboard = () => {
   return (
     <div>
-      <UserButton/>
+      <Header/>
     </div>
   )
 }
