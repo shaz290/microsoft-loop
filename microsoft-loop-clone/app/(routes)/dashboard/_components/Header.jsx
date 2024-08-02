e611db489836b23db60ffa8de0c134c9361d3a1c
shaz.ahmed <steve.josh@kanrad.com>
2024-08-02 14:05:21 +0530

    added user detail insetion in firebase

--- microsoft-loop-clone/app/(routes)/dashboard/_components/Header.jsx
+++ microsoft-loop-clone/app/(routes)/dashboard/_components/Header.jsx
@@ -1,12 +1,33 @@
 "use client"
 import { Logo } from '@/app/_components/Logo'
-import { OrganizationSwitcher, useAuth, UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { db } from '@/config/firebaseConfig'
+import { OrganizationSwitcher, useAuth, UserButton, useUser } from '@clerk/nextjs'
+import { doc, setDoc } from 'firebase/firestore'
+import React, { useEffect } from 'react'
 
 const Header = () => {
   const { orgId } = useAuth();
   console.log(orgId);
+  const { user } = useUser();
 
+  useEffect(() => {
+    user && saveUserData();
+  }, [user])
+
+  // Save UserData
+
+  const saveUserData = async () => {
+    const docID = user?.primaryEmailAddress?.emailAddress;
+    try {
+      await setDoc(doc(db, 'LoppUsers', docID), {
+        name: user?.firstName,
+        avatar: user?.imageUrl,
+        email: user?.primaryEmailAddress?.emailAddress
+      })
+    } catch (e) {
+
+    }
+  }
   return (
     <div className='flex justify-between items-center p-3
     shadow-sm '>
